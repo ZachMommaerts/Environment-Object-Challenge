93dfb1879750b4f28c70a7d51a64dfd5cffdf095
ZachMommaerts <zmommaerts@gmail.com>
2023-03-31 12:41:49 -0500

    Initial Commit

---
+++ Environment Object Challenge/Environment_Object_ChallengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  Environment_Object_ChallengeApp.swift
+//  Environment Object Challenge
+//
+//  Created by Zach Mommaerts on 3/31/23.
+//
+
+import SwiftUI
+
+@main
+struct Environment_Object_ChallengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
